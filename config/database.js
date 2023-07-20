36974cedae148c0003fbed0757cf12dae86938d9
ᎯᏞ ᎷᎯᎻᎠᎥ  ᏃᎯᏞᏫᏌᏦ <86550866+Mahdi-LY@users.noreply.github.com>
2023-07-20 19:45:19 +0200

    change database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,24 @@
 const { createPool } = require('mysql')
 
 const pool = createPool(
-{
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.MYSQL_PORT,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Al mahdiMySql',
-  database: 'course_center',
-  // connectionLimit: 10
-})
+  {
+    host: process.env.DB_HOST || 'aws.connect.psdb.cloud',
+    port: process.env.MYSQL_PORT,
+    user: process.env.DB_USER || 'rjfxcnq2ei4pz5xabxfo',
+    password: process.env.DB_PASSWORD || 'pscale_pw_ypxjJKtxT1Ftsudg8ZlUUqRIs9CZ9fTWs4dz00r9Hfe',
+    database: 'coursecenter',
+    // connectionLimit: 10
+  })
+
+  // const pool = createPool(
+// {
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || 'Al mahdiMySql',
+//   database: 'course_center',
+//   // connectionLimit: 10
+// })
 
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST || 'localhost',
